455cf99c9428c95e2607498c3c3d60193ba02173
Luiz Henrique Goncalves <hen.rick.17@hotmail.com>
2021-03-24 20:51:25 -0300

    Models

---
+++ 8Puzzle/8Puzzle/Models/Posicao.cs
@@ -0,0 +1,16 @@
+﻿namespace AStar8Puzzle
+{
+    public class Posicao
+    {
+        public int Linha { get; set; }
+        public int Coluna { get; set; }
+
+        public Posicao() { }
+
+        public Posicao(int linha, int coluna)
+        {
+            Linha = linha;
+            Coluna = coluna;
+        }
+    }
+}
