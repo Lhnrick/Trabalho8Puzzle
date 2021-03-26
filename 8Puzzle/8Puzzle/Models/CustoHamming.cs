6a233e07eb42ad945a181739fa44f50948c03a35
Matheus de Oliveira de Andrade <mathnhais@outlook.com>
2021-03-25 22:47:53 -0300

    Correção

--- 8Puzzle/8Puzzle/Models/CustoHamming.cs
+++ 8Puzzle/8Puzzle/Models/CustoHamming.cs
@@ -2,50 +2,21 @@ using System;
 
 namespace _8Puzzle.Models
 {
-    public class CustoManhattan
+    public class CustoHamming
     {
-        private int DistanciaManhattan(No noAtual, No noObjetivo)
+        public int CompararEstados(int[][] estado, int[][] estadoObjetivo)
         {
-            int totalQuadrados = noAtual.EstadoAtual.GetLength(0) * noAtual.EstadoAtual.GetLength(1);
-            int[] noAtualarr = new int[totalQuadrados];
-            int contAux = 0;
-            for (int i = 0; i < noAtual.EstadoAtual.GetLength(0); i++)
-                for (int j = 0; j < noAtual.EstadoAtual.GetLength(1); j++)
-                {
-                    noAtualarr[contAux] = noAtual.EstadoAtual[i][j];
-                    contAux++;
-                }
-
-            int distance = 0;
-            for (int i = 0; i < totalQuadrados; i++)
-            {
-                for (int j = 0; j < totalQuadrados; j++)
-                {
-                    int valorAtual = noAtual.EstadoAtual[i][j];
-                    if (valorAtual != 0)
-                    {
-                        var posicao = EncontrarValor(noObjetivo.EstadoAtual, valorAtual);
-                        distance += Math.Abs(i - posicao.Linha) + Math.Abs(j - posicao.Coluna);
-                    }
-                }
-            }
-            return distance;
-        }
-
-        private Posicao EncontrarValor(int[][] estado, int valor)
-        {
+            int localerrado = 0;
             for (int i = 0; i < estado.GetLength(0); i++)
             {
                 for (int j = 0; j < estado.GetLength(1); j++)
                 {
-                    if (estado[i][j] == valor)
-                    {
-                        return new Posicao(i, j);
-                    }
+
+                    if (estado[i][j] != estadoObjetivo[i][j])
+                        localerrado++;
                 }
             }
-            return null;
+            return localerrado;
         }
-
     }
 }
